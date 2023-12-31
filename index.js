92ebfdbd02848ee6f09201d342c515e02cab0e31
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-12-31 10:28:28 +0530

    Add the other html files's url paths

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ const url = require('node:url')
 http.createServer((req, res) => {
     if (url.parse(req.url, true).pathname !== '/favicon.ico' && url.parse(req.url, true).pathname !== '/') {
         let filename = url.parse(req.url, true).pathname.slice(1) + '.html';
+        fs.readFile(filename, 'utf-8', (err, data) => {
+            if (err) {
+                fs.readFile('404.html', 'utf-8', (err, edata) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' })
+                    res.write(edata)
+                    return res.end()
+                })
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(data)
+            return res.end()
+        })
     }
-    else if(url.parse(req.url, true).pathname === '/'){
-        fs.readFile('index.html' , 'utf-8' , (err , data) => {
-            if(err){
-                fs.readFile('404.html' ,'utf-8' , (err,edata) => {
-                    res.writeHead(404 , { 'Content-Type' : 'text/html' })
-                    res.write(data)
+    else if (url.parse(req.url, true).pathname === '/') {
+        fs.readFile('index.html', 'utf-8', (err, data) => {
+            if (err) {
+                fs.readFile('404.html', 'utf-8', (err, edata) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' })
+                    res.write(edata)
                     res.end()
                 })
             }
-            res.writeHead(200 , { 'Content-Type' : 'text/html' })
+            res.writeHead(200, { 'Content-Type': 'text/html' })
             res.write(data)
             return res.end()
         })
