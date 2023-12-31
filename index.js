1fb4b74ac5d0d8391d6bdefccc16416bd191b2d0
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-12-31 10:09:22 +0530

    Change the res-code in error for index.html to 404

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const url = require('node:url')
 http.createServer((req, res) => {
     if (url.parse(req.url, true).pathname !== '/favicon.ico' && url.parse(req.url, true).pathname !== '/') {
         let filename = url.parse(req.url, true).pathname.slice(1) + '.html';
-        res.writeHead(200 , { 'Content-Type' : 'text/html' })
-        console.log(filename)
-        return res.end()
     }
     else if(url.parse(req.url, true).pathname === '/'){
         fs.readFile('index.html' , 'utf-8' , (err , data) => {
             if(err){
                 fs.readFile('404.html' ,'utf-8' , (err,edata) => {
-                    res.writeHead(200 , { 'Content-Type' : 'text/html' })
+                    res.writeHead(404 , { 'Content-Type' : 'text/html' })
                     res.write(data)
                     res.end()
                 })
